0f71b1580380dfeb3bcf2fb6b2b7b526dc2897e3
miriam <miriambautis21@gmail.com>
2018-03-23 16:50:08 +0100

    Practice 1

---
+++ openfda1/test.py
@@ -0,0 +1,20 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json?limit=10", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+
+#print("The id of the first drug is", repos["results"][0]["id"])
+#print("The purpose is", repos["results"][0]["purpose"])
+#print("The manufacter is", repos["results"][0]["openfda"]["manufacturer_name"])
+
+for elem in range(len(repos["results"])):
+    print("The id of drug", elem, "is",repos["results"][elem]["id"])
