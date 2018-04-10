dc3739cb85d6e1cb9459616e5d86e06bc222bbcc
m.bautista.2017 <miriambautis21@gmail.com>
2018-04-10 16:57:08 +0200

    second try, me falta cambiar el mensaje a un string

--- openfda3/intento.py
+++ openfda3/intento.py
@@ -1,17 +1,14 @@
 import http.server
 import socketserver
 import json
-import http.client
 
-# -- IP and the port of the server
-IP = "localhost"  # Localhost means "I": your local machine
-PORT = 8002
-
+PORT = 8006
 
 # HTTPRequestHandler class
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     # GET
     def do_GET(self):
+
         # Send response status code
         self.send_response(200)
 
@@ -20,44 +17,37 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
 
         headers = {'User-Agent': 'http-client'}
-
-        conn = http.client.HTTPSConnection("api.openfda.gov")
+        conn = http.client.HTTPSConnection("api.fda.gov")
         conn.request("GET", "/drug/label.json?limit=10", None, headers)
         r1 = conn.getresponse()
         print(r1.status, r1.reason)
         repos_raw = r1.read().decode("utf-8")
         conn.close()
-        print("hola")
 
         d_labelling = json.loads(repos_raw)
 
-        # Send message back to clien
+        #d_labelling = d_labelling['results']
+        #drug_id = "<ol>" + d_labelling[0]['id'] + "<\ol>"
 
-        for elem in range(len(d_labelling["results"])):
-            drugs_id = "<ol>" + d_labelling["resulst"][elem]["id"] + "<\ol>"
-            print(drugs_id)
+        #message =[]
 
+        #for elem in range(len(d_labelling["results"])):
+            #drugs_id = "<ol>" + d_labelling["results"][elem]["id"] + "<\ol>"
+            #print(drugs_id)
 
+        # Send message back to client
+        message =
+            for elem in range(len(d_labelling["results"])):
+                drugs_id = "<ol>" + d_labelling["results"][elem]["id"] + "<\ol>"
 
-# Write content as utf-8 data
+        # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
-        print("File served!")
         return
 
-
-# Handler = http.server.SimpleHTTPRequestHandler
+Handler = http.server.SimpleHTTPRequestHandler
 Handler = testHTTPRequestHandler
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
-try:
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    pass
+httpd.serve_forever()
 
-httpd.server_close()
-print("")
-print("Server stopped!")
-
-
-# https://github.com/joshmaker/simple-python-webserver/blob/master/server.py
