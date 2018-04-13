de04f7daaf970ec69b63c94c2aafdb54103e5c01
m.bautista.2017 <miriambautis21@outlook.com>
2018-04-13 17:01:43 +0200

    practice 4

--- openfda-4/p4intento.py
+++ openfda-4/p4intento.py
@@ -1,7 +1,7 @@
 import http.server
 import socketserver
+import http.client
 import json
-
 
 PORT = 8000
 
@@ -9,16 +9,8 @@ PORT = 8000
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     # GET
     def do_GET(self):
-        headers = {'User-Agent': 'http-client'}
 
-        conn = http.client.HTTPSConnection("api.fda.gov")
-        conn.request("GET", "/drug/label.json?limit=10", None, headers)
-        r1 = conn.getresponse()
-        print(r1.status, r1.reason)
-        repos_raw = r1.read().decode("utf-8")
-        conn.close()
-
-        # Send response status code
+       # Send response status code
         self.send_response(200)
 
         # Send headers
@@ -26,9 +18,34 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
 
         # Send message back to client
-        message = drug_id
-        # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8")) #esto es lo que le llega al cliente
+        message = ''
+        if self.path == "/":
+            with open("searchfda4.html", "r") as s: #we open search
+                message = s.read()#lee search y lo ejecuta
+            self.wfile.write(bytes(message, "utf8")) #esto es lo que le llega al cliente
+        #en caso de que tenga más parametros que solo \
+        elif "search" in self.path:
+            parame = self.path.split("?")[1]
+            label= parame.split("&")[0].split("=")[1]
+            limit= parame.split("&")[1].split("=")[1]
+
+        headers = {'User-Agent': 'http-client'}
+
+        conn = http.client.HTTPSConnection("api.fda.gov")
+        conn.request("GET", "/drug/label.json?search=generic_name:" +label+ "&limit=" + limit , None, headers)
+        r1 = conn.getresponse()
+        print(r1.status, r1.reason)
+        repos_raw = r1.read().decode("utf-8")
+        conn.close()
+
+        d_labelling = json.loads(repos_raw)
+
+        drug_id = ''
+        for elem in range(len(d_labelling["results"])):
+            drug_id = drug_id + "<ol>" + d_labelling["results"][elem]["id"] + "</ol>"
+
+        self.wfile.write(bytes(drug_id, "utf8"))
+
         return
 
     #search este dentro de self.path
---
+++ openfda-4/xd.py
@@ -0,0 +1,5 @@
+row =
+row = row.strip("serch?").split("&")
+parameters = row.split("=")
+label = parameters[1]
+limit = parameters[3]
