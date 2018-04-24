4bd04c5f4f51992400dc46b14076ed715f019242
m.bautista.2017 <miriambautis21@gmail.com>
2018-04-24 15:40:05 +0200

    first try basic practice

---
+++ openfda-project/basic 5.py
@@ -0,0 +1,56 @@
+import http.server
+import socketserver
+import http.client
+import json
+
+socketserver.TCPServer.allow_reuse_adress = True
+
+PORT = 8000
+
+# HTTPRequestHandler class
+class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    # GET
+    def do_GET(self):
+
+       # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = ''
+        if self.path == "/":
+            with open("searchfda4.html", "r") as s: #we open search
+                message = s.read()#lee search y lo ejecuta
+            self.wfile.write(bytes(message, "utf8")) #esto es lo que le llega al cliente
+        #en caso de que tenga más parametros que solo \
+        elif "search" in self.path:
+            parame = self.path.split("?")[1]
+            drug = parame.split("&")[0].split("=")[1]
+            limit= parame.split("&")[1].split("=")[1]
+
+            headers = {'User-Agent': 'http-client'}
+
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            conn.request("GET", "/drug/label.json?search=generic_name:" + drug + "&limit=" + limit , None, headers)
+            r1 = conn.getresponse()
+            print(r1.status, r1.reason)
+            repos_raw = r1.read().decode("utf-8")
+            conn.close()
+
+            d_labelling = json.loads(repos_raw)
+            self.wfile.write(bytes(str(d_labelling), "utf8"))
+
+        return
+
+    #search este dentro de self.path
+
+Handler = http.server.SimpleHTTPRequestHandler
+Handler = testHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
+httpd.serve_forever()
+
