f88e14cecfbe691ca8cfbae36bf7a4f0f692293d
m.bautista.2017 <miriambautis21@gmail.com>
2018-04-28 14:52:09 +0200

    try

--- openfda-project/basic 5.py
+++ openfda-project/basic 5.py
@@ -47,11 +47,12 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         elif "searchDrug" in self.path:
             parame =self.path.split("?")[1]
             active_ingredient = parame.split("=")[1]
+            limit = parame.split("&")[1].split("=")[1]
 
             headers = {'User-Agent': 'http-client'}
 
             conn = http.client.HTTPSConnection("api.fda.gov")
-            conn.request("GET", "/drug/label.json?search=active_ingredient=" + active_ingredient, None, headers)
+            conn.request("GET", "/drug/label.json?search=active_ingredient=" + active_ingredient + "&limit=" + limit, None, headers)
             r1 = conn.getresponse()
             print(r1.status, r1.reason)
             repos_raw = r1.read().decode("utf-8")
@@ -67,11 +68,12 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         elif "searchCompany" in self.path:
             parame =self.path.split("?")[1]
             company_name = parame.split("=")[1]
+            limit = parame.split("&")[1].split("=")[1]
 
             headers = {'User-Agent': 'http-client'}
 
             conn = http.client.HTTPSConnection("api.fda.gov")
-            conn.request("GET", "/drug/label.json?search=openfda.manufacturer_name=" + company_name, None, headers)
+            conn.request("GET", "/drug/label.json?search=openfda.manufacturer_name:" + company_name + "&limit=" + limit, None, headers)
             r1 = conn.getresponse()
             print(r1.status, r1.reason)
             repos_raw = r1.read().decode("utf-8")
@@ -79,18 +81,19 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
             d_labelling = json.loads(repos_raw)
 
-            for elem in range(len(d_labelling["results"])):
+            for elem in range(len(d_labelling['results'])):
                 drug_mn = "<li>" + d_labelling["results"][elem]["openfda"]["brand_name"][0] + "</li>"
 
                 self.wfile.write(bytes(str(drug_mn), "utf8"))
-        elif "limit":
+
+        elif "listDrug" in self.path:
             parame = self.path.split("?")[1]
-            listd = parame.split("=")[1]
+            listdrug = parame.split("=")[1]
 
             headers = {'User-Agent': 'http-client'}
 
             conn = http.client.HTTPSConnection("api.fda.gov")
-            conn.request("GET", "/drug/label.json?limit=" + listd , None, headers)
+            conn.request("GET", "/drug/label.json?limit=" + listdrug , None, headers)
             r1 = conn.getresponse()
             print(r1.status, r1.reason)
             repos_raw = r1.read().decode("utf-8")
@@ -98,8 +101,30 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
             d_labelling = json.loads(repos_raw)
 
-            for elem in range(len(d_labelling["results"])):
-                drug_mn = "<li>" + d_labelling["results"][elem]["openfda"]["brand_name"][0] + "</li>"
+            for i in range(len(d_labelling["results"])):
+                drug_list = "<li>" + d_labelling["results"][i]["openfda"]["brand_name"][0] + "</li>"
+
+                self.wfile.write(bytes(str(drug_list), "utf8"))
+
+        elif "listCompany" in self.path:
+            parame = self.path.split("?")[1]
+            listcompany = parame.split("=")[1]
+
+            headers = {'User-Agent': 'http-client'}
+
+            conn = http.client.HTTPSConnection("api.fda.gov")
+            conn.request("GET", "/drug/label.json?limit=" + listcompany , None, headers)
+            r1 = conn.getresponse()
+            print(r1.status, r1.reason)
+            repos_raw = r1.read().decode("utf-8")
+            conn.close()
+
+            d_labelling = json.loads(repos_raw)
+
+            for i in range(len(d_labelling["results"])):
+                company_list = "<li>" + d_labelling["results"][i]["openfda"]["manufacturer_name"][0] + "</li>"
+
+                self.wfile.write(bytes(str(company_list), "utf8"))
 
         return
 
